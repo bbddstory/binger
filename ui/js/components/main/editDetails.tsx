650223e8abdea687acd21c3c4e440018365d53fa
LAUREL\Meleon <bbddstory@gmail.com>
2018-03-30 02:18:24 +1100

    Finished editDetails UI

--- ui/js/components/main/editDetails.tsx
+++ ui/js/components/main/editDetails.tsx
@@ -19,75 +19,44 @@ class EditDetails extends React.Component<any, any> {
 
     return (
       <div className="popup-bg">
-        <div className="edit-details">
-          <form action="">
-            
+        <div className="popup-panel">
+          <div className="panel-body">
+            <form action="">
               <label>English Title</label><input type="text" placeholder="N/A" />
-            
-            
               <label>Original Title</label><input type="text" placeholder="N/A" />
-            
-            
-              <label>Year</label><input type="text" width="4" className="year" placeholder="N/A" />
-            
-            
-              <label>Director</label><input type="text" placeholder="N/A" />
-            
-            
-              <label>Runtime</label><input type="text" className="runtime" placeholder="N/A" />
-            
-            
+              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <div style={{ width: '23%' }}>
+                  <label>Year</label><input type="text" width="4" className="year" placeholder="N/A" />
+                </div>
+                <div style={{ width: '23%' }}>
+                  <label>Runtime</label><input type="text" className="runtime" placeholder="N/A" />
+                </div>
+                <div style={{ width: '49%' }}>
+                  <label>Director</label><input type="text" placeholder="N/A" />
+                </div>
+              </div>
               <label>Poster</label><input type="text" placeholder="N/A" />
-            
-            
               <label className="textarea-lbl">Plot</label><textarea placeholder="N/A" />
-            
-            
-              <label>IMDB ID</label><input type="text" className="imdb" placeholder="N/A" />
-            
-            
-              <label>Rating</label><input type="text" className="rating" placeholder="N/A" />
-            
-            
-              <label>Status</label><input type="text" className="rating" placeholder="N/A" />
-            
-            
-              <label>Type</label><input type="text" placeholder="N/A" />
-            
-            {/* <div className="row">
-              <label>English Title</label><input type="text" placeholder="N/A" />
-            </div>
-            <div className="row">
-              <label>Original Title</label><input type="text" placeholder="N/A" />
-            </div>
-            <div className="row">
-              <label>Year</label><input type="text" width="4" className="year" placeholder="N/A" />
-            </div>
-            <div className="row">
-              <label>Director</label><input type="text" placeholder="N/A" />
-            </div>
-            <div className="row">
-              <label>Runtime</label><input type="text" className="runtime" placeholder="N/A" />
-            </div>
-            <div className="row">
-              <label>Poster</label><input type="text" placeholder="N/A" />
-            </div>
-            <div className="row">
-              <label className="textarea-lbl">Plot</label><textarea placeholder="N/A" />
-            </div>
-            <div className="row">
-              <label>IMDB ID</label><input type="text" className="imdb" placeholder="N/A" />
-            </div>
-            <div className="row">
-              <label>Rating</label><input type="text" className="rating" placeholder="N/A" />
-            </div>
-            <div className="row">
-              <label>Status</label><input type="text" className="rating" placeholder="N/A" />
-            </div>
-            <div className="row">
-              <label>Type</label><input type="text" placeholder="N/A" />
-            </div> */}
-          </form>
+              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <div style={{ width: '23%' }}>
+                  <label>IMDB ID</label><input type="text" className="imdb" placeholder="N/A" />
+                </div>
+                <div style={{ width: '23%' }}>
+                  <label>Rating</label><input type="text" className="rating" placeholder="N/A" />
+                </div>
+                <div style={{ width: '23%' }}>
+                  <label>Status</label><input type="text" className="rating" placeholder="N/A" />
+                </div>
+                <div style={{ width: '23%' }}>
+                  <label>Type</label><input type="text" placeholder="N/A" />
+                </div>
+              </div>
+            </form>
+          </div>
+          <div className="panel-footer">
+            <button>Cancel</button>
+            <button>OK</button>
+          </div>
         </div>
       </div>
     )
