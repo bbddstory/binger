f4b1dd750e1efbab82dafdb04f5af336a401da37
bbddstory <bbddstory@live.com>
2018-04-27 01:09:42 +1000

    Downgraded version numbers in package.json, incompatibility issue. Removed redux-form, going to use my own form checking.

--- ui/js/reducers/masterReducer.ts
+++ ui/js/reducers/masterReducer.ts
@@ -4,10 +4,8 @@ import { combineReducers } from 'redux';
 import { uiReducer } from './uiReducer';
 import { loginReducer } from './loginReducer';
 import { dataReducer } from './dataReducer';
-import { reducer as reduxFormReducer } from 'redux-form';
 
 export default combineReducers({
-  form: reduxFormReducer, // mounted under "form"
   uiReducer,
   loginReducer,
   dataReducer
--- ui/js/views/main/editDetails.tsx
+++ ui/js/views/main/editDetails.tsx
@@ -54,19 +54,19 @@ class EditDetails extends React.Component<any, any> {
 
     return (
       <div className="popup-bg">
-        {/* <LocalForm onChange={(values) => this.onChange(values)} onSubmit={(values) => this.onSubmit(values)}>
+        <form onChange={(values) => this.onChange(values)} onSubmit={(values) => this.onSubmit(values)}>
           <div className="popup-panel">
             <div className="panel-body">
 
               <div style={{ display: 'flex', justifyContent: 'space-between' }}>
                 <div style={{ width: '48.5%' }}>
                   <label>English Title</label>
-                  <Control.text model=".engTitle" placeholder="N/A" defaultValue={item.engTitle} />
+                  <input type="text" placeholder="N/A" defaultValue={item.engTitle} />
                   {this.state.engTitle && <span>Contains invalid characters</span>}
                 </div>
                 <div style={{ width: '49%' }}>
                   <label>Original Title</label>
-                  <Control.text model=".origTitle" placeholder="N/A" defaultValue={item.origTitle} />
+                  <input type="text" placeholder="N/A" defaultValue={item.origTitle} />
                   {this.state.origTitle && <span>Contains invalid characters</span>}
                 </div>
               </div>
@@ -74,17 +74,17 @@ class EditDetails extends React.Component<any, any> {
               <div style={{ display: 'flex', justifyContent: 'space-between' }}>
                 <div style={{ width: '22.9%' }}>
                   <label>Year</label>
-                  <Control.text model=".year" placeholder="N/A" defaultValue={item.year} />
+                  <input type="text" placeholder="N/A" defaultValue={item.year} />
                   {this.state.year && <span>Must be 4 digits</span>}
                 </div>
                 <div style={{ width: '22.9%' }}>
                   <label>Runtime</label>
-                  <Control.text model=".runtime" placeholder="N/A" defaultValue={item.runtime} />
+                  <input type="text" placeholder="N/A" defaultValue={item.runtime} />
                   {this.state.runtime && <span>Format: 1h 30min</span>}
                 </div>
                 <div style={{ width: '49%' }}>
                   <label>Stars</label>
-                  <Control.text model=".stars" placeholder="N/A" defaultValue={item.stars} />
+                  <input type="text" placeholder="N/A" defaultValue={item.stars} />
                   {this.state.director && <span>One or more names separated by comma</span>}
                 </div>
               </div>
@@ -92,39 +92,39 @@ class EditDetails extends React.Component<any, any> {
               <div style={{ display: 'flex', justifyContent: 'space-between' }}>
                 <div style={{ width: '48.5%' }}>
                   <label>Director</label>
-                  <Control.text model=".director" placeholder="N/A" defaultValue={item.director} />
+                  <input type="text" placeholder="N/A" defaultValue={item.director} />
                   {this.state.director && <span>One or more names separated by comma</span>}
                 </div>
                 <div style={{ width: '49%' }}>
                   <label>Creator</label>
-                  <Control.text model=".creator" placeholder="N/A" defaultValue={item.creator} />
+                  <input type="text" placeholder="N/A" defaultValue={item.creator} />
                   {this.state.director && <span>One or more names separated by comma</span>}
                 </div>
               </div>
 
               <label className="textarea-lbl">Plot</label>
-              <Control.textarea model=".plot" className="plot" placeholder="N/A" defaultValue={item.plot} />
+              <textarea className="plot" placeholder="N/A" defaultValue={item.plot} />
               {this.state.plot && <span>Contains invalid characters</span>}
 
               <div style={{ display: 'flex', justifyContent: 'space-between' }}>
                 <div style={{ width: '23%' }}>
                   <label>IMDB ID</label>
-                  <Control.text model=".imdb_id" placeholder="N/A" defaultValue={item.imdb_id} />
+                  <input type="text" placeholder="N/A" defaultValue={item.imdb_id} />
                   {this.state.imdb_id && <span>Format: tt1234567</span>}
                 </div>
                 <div style={{ width: '23%' }}>
                   <label>Rating</label>
-                  <Control.text model=".rating" placeholder="N/A" defaultValue={item.rating} />
+                  <input type="text" placeholder="N/A" defaultValue={item.rating} />
                   {this.state.rating && <span>Format: 9.9</span>}
                 </div>
                 <div style={{ width: '23%' }}>
                   <label>Douban</label>
-                  <Control.text model=".douban" placeholder="N/A" defaultValue={item.douban} />
+                  <input type="text" placeholder="N/A" defaultValue={item.douban} />
                   {this.state.imdb_id && <span>Format: tt1234567</span>}
                 </div>
                 <div style={{ width: '23%' }}>
                   <label>Mtime</label>
-                  <Control.text model=".mtime" placeholder="N/A" defaultValue={item.mtime} />
+                  <input type="text" placeholder="N/A" defaultValue={item.mtime} />
                   {this.state.rating && <span>Format: 9.9</span>}
                 </div>
               </div>
@@ -132,17 +132,17 @@ class EditDetails extends React.Component<any, any> {
               <div style={{ display: 'flex', justifyContent: 'space-between' }}>
                 <div style={{ width: '23%' }}>
                   <label>Trailer</label>
-                  <Control.text model=".trailer" placeholder="N/A" defaultValue={item.trailer} />
+                  <input type="text" placeholder="N/A" defaultValue={item.trailer} />
                   {this.state.imdb_id && <span>Format: tt1234567</span>}
                 </div>
                 <div style={{ width: '23%' }}>
                   <label>Featurette</label>
-                  <Control.text model=".featurette" placeholder="N/A" defaultValue={item.featurette} />
+                  <input type="text" placeholder="N/A" defaultValue={item.featurette} />
                   {this.state.rating && <span>Format: 9.9</span>}
                 </div>
                 <div style={{ width: '23%' }}>
                   <label>Status</label>
-                  <Control.select model=".status" defaultValue={item.status}>
+                  <select defaultValue={item.status}>
                     <option value="0">0</option>
                     <option value="1">1</option>
                     <option value="2">2</option>
@@ -153,25 +153,25 @@ class EditDetails extends React.Component<any, any> {
                     <option value="7">7</option>
                     <option value="8">8</option>
                     <option value="9">9</option>
-                  </Control.select>
+                  </select>
                 </div>
                 <div style={{ width: '23%' }}>
                   <label>Category</label>
-                  <Control.select ref="catSel" model=".cat" defaultValue={item.cat} disabled={!this.props.uiState.newRec}>
+                  <select ref="catSel" defaultValue={item.cat} disabled={!this.props.uiState.newRec}>
                     <option value={cats.MOVIE}>Movie</option>
                     <option value={cats.TV}>TV</option>
                     <option value={cats.DOC}>Documentary</option>
                     <option value={cats.ANIME}>Animation</option>
-                  </Control.select>
+                  </select>
                 </div>
               </div>
 
               <label>Poster</label>
-              <Control.textarea model=".poster" className="poster" placeholder="N/A" defaultValue={item.poster} />
+              <textarea className="poster" placeholder="N/A" defaultValue={item.poster} />
               {this.state.poster && <span>Must be a valid URL</span>}
 
               <label>Subtitle</label>
-              <Control.text model=".subtitle" placeholder="N/A" defaultValue={item.subtitle} />
+              <input type="text" placeholder="N/A" defaultValue={item.subtitle} />
               {this.state.subtitle && <span>Must be a valid URL</span>}
 
             </div>
@@ -180,7 +180,7 @@ class EditDetails extends React.Component<any, any> {
               <button className="btn-main" type="submit">Save</button>
             </div>
           </div>
-        </LocalForm> */}
+        </form>
       </div>
     )
   }
