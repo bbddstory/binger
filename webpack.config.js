b4e3e6b9ebbe58ca1d7afb87ed2bb63a415443ae
UNICO-AUS\lli <leon.li@unico.com.au>
2018-06-08 16:09:51 +1000

    Migrated Webpack from 3 to 4. Going to study the new version, and revamp the config file. Reduced login background image size.

--- webpack.config.js
+++ webpack.config.js
@@ -1,74 +1,83 @@
-const buildPath = 'build';
 const path = require('path');
-// const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
-const ccp = require('./node_modules/webpack/lib/optimize/CommonsChunkPlugin');
-const CommonsChunkConfig = new ccp({
-  name: 'vendor',
-  filename: 'js/vendor.bundle.js'
-});
-// Extracts all imported css from javascript imports into a css file
-// for parallel loading of CSS and JS in the browsers
-const ExtractTextWebpackPluginConfig = new ExtractTextWebpackPlugin({
-  filename: 'style.bundle.css',
-  allChunks: true
-});
-const StyleLoaderConfig = {
-  dev: ExtractTextWebpackPlugin.extract({
-    fallback: 'style-loader?sourceMap',
-    use: [
-      'css-loader',
-      'sass-loader'
-    ]
-  })
-};
-const CopyWebpackPluginConfig = new CopyWebpackPlugin([{
-  from: path.resolve(__dirname, 'ui/index.html')
-}, {
-  from: path.resolve(__dirname, 'ui/images/favicon.ico'),
-  to: 'images'
-}]);
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
-    vendor: [
+    app: './ui/js/views/index.tsx',
+    vendors: [
       'react',
       'redux',
       'react-redux'
-    ],
-    app: [
-      // This is app root, all other JS are imported
-      './ui/js/views/index.tsx'
     ]
   },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new OptimizeCSSAssetsPlugin({})
+    ],
+    splitChunks: {
+      chunks: "async",
+      minSize: 30000,
+      minChunks: 1,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 3,
+      automaticNameDelimiter: '~',
+      name: true,
+      cacheGroups: {
+        commons: {
+          name: "commons",
+          test: /[\\/]node_modules[\\/]/,
+          chunks: "all"
+        },
+        styles: {
+          name: 'styles',
+          test: /\.css$/,
+          chunks: 'all',
+          enforce: true
+        }
+      }
+    }
+  },
   // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
+  // devtool: "source-map",
   resolve: {
-    // Add '.ts' and '.tsx' as resolvable extensions.
+    // Resolvable extensions
     extensions: [".ts", ".tsx", ".js", ".json"]
   },
   output: {
-    path: path.join(__dirname, buildPath), // needs to be an absolute path
+    path: path.join(__dirname, 'dist'), // needs to be an absolute path
     // publicPath: '../',
     filename: 'js/[name].bundle.js'
   },
   module: {
     rules: [
-      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      // All '.ts' and '.tsx' files are handled by 'awesome-typescript-loader'
       {
         test: /\.tsx?$/,
-        loader: "awesome-typescript-loader"
+        loader: "ts-loader"
       },
-
-      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      // All '.js' and '.jsx' files are handled by 'babel-loader'
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules)/,
+        query: {
+          presets: ['env', 'react']
+        }
+      },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'
       {
         enforce: "pre",
         test: /\.js$/,
         loader: "source-map-loader"
       },
+      // All '.html' files are handled by 'html-loader'
       {
-        test: /\.(html)$/,
+        test: /\.html$/,
         use: [{
           loader: 'html-loader',
           options: {
@@ -77,24 +86,28 @@ module.exports = {
             // collapseWhitespace: true
           }
         }],
-      }, {
-        test: /\.(js|jsx)$/,
-        loader: 'babel-loader',
-        exclude: /(node_modules)/,
-        query: {
-          presets: ['env', 'react']
-        }
-      }, {
-        test: /\.(css|scss)$/,
-        use: StyleLoaderConfig.dev
-      }, {
+      },
+      // All '.sass', '.scss' and '.css' files are handled here
+      {
+        test: /\.s?[ac]ss$/,
+        use: [
+          true ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader',
+          'sass-loader'
+          // { loader: "style-loader" },
+          // { loader: "css-loader" },
+          // { loader: "sass-loader" }
+        ],
+      },
+      {
         test: /\.(otf|eot|ttf|woff|woff2)$/,
         loader: 'file-loader',
         options: {
           name: 'fonts/[name].[ext]'
         }
-      }, {
-        test: /\.(jpg|png|gif|bmp|ico|svg)$/,
+      },
+      {
+        test: /\.(jpg|png|svg|gif|bmp|ico)$/,
         loader: 'file-loader',
         options: {
           name: 'images/[name].[ext]'
@@ -103,8 +116,21 @@ module.exports = {
     ]
   },
   plugins: [
-    CommonsChunkConfig,
-    ExtractTextWebpackPluginConfig,
-    CopyWebpackPluginConfig
+    new CopyWebpackPlugin([{
+      from: path.resolve(__dirname, 'ui/index.html')
+    }, {
+      from: path.resolve(__dirname, 'ui/images/favicon.ico'),
+      to: 'images'
+    }]),
+    new UglifyJsPlugin({
+      test: /\.js($|\?)/i,
+      exclude: /(node_modules)/
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: true ? 'styles.bundle.css' : '[name].[hash].css',
+      chunkFilename: true ? '[id].css' : '[id].[hash].css'
+    })
   ]
 };
